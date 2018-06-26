31710d1fb4b31159866c970ef085297cb12caa1e
RyanSchu <rschubert1@luc.edu>
2018-06-26 15:56:16 -0500

    fixed interpolation error

--- codes/pre_eqtl_filtering.R
+++ codes/pre_eqtl_filtering.R
@@ -1,8 +1,8 @@
 library(dplyr)
 for (i in 1:22){
 
-  sample_expressions <- as.data.frame(read.table(file = paste("/home/ryan/test_runs/juncfiles/Gene_expression_chr","1",sep=""), sep = '\t', header =T))
-  reference_genotype <- as.data.frame(read.table(file = paste("/home/ryan/DATA/SNP_genotype_chr","1",".txt",sep=""), header=T))
+  sample_expressions <- as.data.frame(read.table(file = paste("/home/ryan/test_runs/juncfiles/Gene_expression_chr",i,sep=""), sep = '\t', header =T))
+  reference_genotype <- as.data.frame(read.table(file = paste("/home/ryan/DATA/SNP_genotype_chr",i,".txt",sep=""), header=T))
   sampnames <- colnames(sample_expressions)
   refnames <- colnames(reference_genotype)
   filtnames <- base::intersect (sampnames, refnames)
