427871018a7f86e97f64b2bf2fa2e44c80116574
Ryan Schubert <31641912+RyanSchu@users.noreply.github.com>
2018-07-24 09:42:36 -0500

    Update SNP_subset.R

--- codes/filtering/SNP_subset.R
+++ codes/filtering/SNP_subset.R
@@ -15,5 +15,6 @@ print("finished subsetting")
 for (i in 1:22){
   SNP_location<-as.data.frame(read.table(file = paste('/home/ryan/Data/SNPdata/SNP_Location_chr',i,'.txt', sep =''), sep='\t', header = T))
   write.table(x = semi_join(samp_genotype, SNP_location), file = paste('/home/ryan/Data/SNPdata/SNP_genotype_chr',i,'.txt', sep=''))
+  write.table(x = semi_join(samp_genotype, SNP_location), file = paste('/home/ryan/Data/SNPdata/SNP_Location_chr',i,'.txt', sep =''))
   print(paste("chr",i,"processed"))
 }
