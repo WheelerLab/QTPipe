763e56cddd962f26a566da0fa5dc7554c49b6521
RyanSchu <rschubert1@luc.edu>
2018-07-25 10:39:27 -0500

    filters snps with unchanging genotype

--- codes/filtering/SNP_subset.R
+++ codes/filtering/SNP_subset.R
@@ -9,12 +9,12 @@ sampnames<-samplelist[,1]
 sampsubset<-base::intersect(refnames, sampnames)
 samp_genotype<-select(SNP_genotype, "rsid", sampsubset)
 samp_genotype<-samp_genotype[rowMeans(samp_genotype[-1]) != 0 
-                            |rowMeans(samp_genotype[-1]) != 1
-                            |rowMeans(samp_genotype[-1]) != 2, ]
+                           & rowMeans(samp_genotype[-1]) != 1
+                           & rowMeans(samp_genotype[-1]) != 2, ]
 print("finished subsetting")
 for (i in 1:22){
   SNP_location<-as.data.frame(read.table(file = paste('/home/ryan/Data/SNPdata/SNP_Location_chr',i,'.txt', sep =''), sep='\t', header = T))
-  write.table(x = semi_join(samp_genotype, SNP_location), file = paste('/home/ryan/Data/SNPdata/SNP_genotype_chr',i,'.txt', sep=''))
-  write.table(x = semi_join(samp_genotype, SNP_location), file = paste('/home/ryan/Data/SNPdata/SNP_Location_chr',i,'.txt', sep =''))
+  write.table(x = semi_join(samp_genotype, SNP_location), file = paste('/home/ryan/Data/SNPdata/SNP_genotype_chr',i,'.txt', sep=''), row.names = F, quote = F)
+  write.table(x = semi_join(SNP_location, samp_genotype), file = paste('/home/ryan/Data/SNPdata/SNP_Location_chr',i,'.txt', sep =''), row.names = F, quote = F)
   print(paste("chr",i,"processed"))
 }
