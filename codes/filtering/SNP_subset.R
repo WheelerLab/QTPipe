af9c2a906615c8b670d24dad2b9bd848bbbc4a77
RyanSchu <rschubert1@luc.edu>
2018-10-02 14:37:39 -0500

    added genotype list output and sample list interpretation

--- codes/filtering/SNP_subset.R
+++ codes/filtering/SNP_subset.R
@@ -14,7 +14,11 @@ SNP_genotype<-as.data.frame(read.table(file = args$snpgenotype, sep='', header =
 print("loaded SNP data")
 refnames <- colnames(SNP_genotype)
 samplelist<-as.data.frame(read.table(file = args$samplelist, sep='\t', header = F))
-sampnames<-make.names(samplelist[,1])
+if (ncol(samplelist) == 2){
+sampnames<-make.names(samplelist[,2])
+} else {
+  sampnames<-make.names(samplelist[,1])
+}
 sampsubset<-base::intersect(refnames, sampnames)
 samp_genotype<-select(SNP_genotype, "id", sampsubset)
 samp_genotype<-samp_genotype[rowMeans(samp_genotype[-1]) != -1 
@@ -22,8 +26,8 @@ samp_genotype<-samp_genotype[rowMeans(samp_genotype[-1]) != -1
                            & rowMeans(samp_genotype[-1]) != 1
                            & rowMeans(samp_genotype[-1]) != 2, ]
 print("finished subsetting")
-
+sampsubset<-gsub('.', '-', sampsubset, fixed = T)
 SNP_location<-as.data.frame(read.table(file = args$snplocation, sep='', header = T))
 write.table(x = semi_join(samp_genotype, SNP_location, by = c("id" = "snp")), file = paste(args$outputdir,"/",args$tag, "SNPgenotype.txt", sep = ''), row.names = F, quote = F)
 write.table(x = semi_join(SNP_location, samp_genotype, by = c("snp" = "id")), file = paste(args$outputdir,"/",args$tag, "SNPlocation.txt", sep = ''), row.names = F, quote = F)
-write.table(x=sampsubset, file = paste(args$outputdir,"/","Genotyped_subset.txt", sep = ''), row.names = F, quote = F)
+write.table(x=sampsubset, file = paste(args$outputdir,"/","Genotyped_subset.txt", sep = ''), row.names = F, quote = F, col.names = F)
