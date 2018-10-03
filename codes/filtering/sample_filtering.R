28e612ad817d7f3120cf755cf8d0219cf0dbd8bf
Ryan Schubert <31641912+RyanSchu@users.noreply.github.com>
2018-10-03 11:29:00 -0500

    Update sample_filtering.R

--- codes/filtering/sample_filtering.R
+++ codes/filtering/sample_filtering.R
@@ -1,9 +1,16 @@
 library(dplyr)
-#argument 1 should be the fastq list with the names dict
-#argument 2 should be the the SNP genotype txt file
-arguments <- commandArgs(trailingOnly = T) 
-sample_list <- read.table(arguments[1],sep='\t',header=F) #read in the samples
-population_list <- read.table(arguments[2],header=F,nrows=1) #read in the population pool
+library(argparse)
+
+parser$add_argument("-s", "--samplelist", help="file path of the sample list")
+parser$add_argument("-sg", "--snpgenotype", help="file path of the snp genotype file")
+parser$add_argument("-o", "--outputdir", help="output directory", type=)
+
+sample_list <- read.table(args$samplelist,sep='\t',header=F) #read in the samples
+population_list <- read.table(args$snpgenotype,header=F,nrows=1) #read in the population pool
 population_list <- unlist(population_list[1,]) #convert population pool into vector
-filtered_list <- filter(sample_list,V1 %in% population_list) #remove any samples that are not in the population pool of our snps
-write.table(x = filtered_list, file = arguments[1], row.names = F, quote = F, col.names = F, sep = '\t') #write the new list to the old file
+if (ncol(samplelist) == 2){
+  filtered_list <- filter(sample_list,V2 %in% population_list) #remove any samples that are not in the population pool of our snps
+} else {
+  filtered_list <- filter(sample_list,V1 %in% population_list) #remove any samples that are not in the population pool of our snps
+}
+write.table(x = filtered_list, file = paste(args$outputdir, "/genotyped_samples.txt"), row.names = F, quote = F, col.names = F, sep = '\t') #write the new list to the old file
