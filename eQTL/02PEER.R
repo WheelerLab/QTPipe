daa98f05012a9bc21df6ece95f44f62a6eb589c4
RyanSchu <rschubert1@luc.edu>
2020-02-24 11:21:47 -0600

    Added testing and margin utilities

--- eQTL/02PEER.R
+++ eQTL/02PEER.R
@@ -2,38 +2,76 @@
 library(peer)
 library(tibble)
 library(dplyr)
-source('~/scripts/rntransform.R')
-source('~/scripts/ztransform.R')
+source('/home/ryan/software/QTPipe/eQTL/rntransform.R')
+source('/home/ryan/software/QTPipe/eQTL/ztransform.R')
 library(preprocessCore)
 library(argparse)
 library(methods)
+library(data.table)
 parser <- ArgumentParser()
 parser$add_argument("-e", "--expression", help="file path of the gene expression file")
 parser$add_argument("-o", "--outputdir", help="output directory", type="character")
 parser$add_argument("-t", "--tag", help="file tag for this run of samples")
 parser$add_argument("--cov", help="file of covariates")
+parser$add_argument("--pilot", help="if present, only tests one level of peer factors instead of iterating.",action="store_true")
+parser$add_argument("--margin",help="1 for rows are individuals, 2 for columns are individuals")
 args <- parser$parse_args()
-expr = read.table(file=args$expression,header=T,check.names = F)
 
-expmat = as.matrix(t(expr[,-1])) #need genes in cols, ids in rows
+check.fread<-function(file_name){
+  zipped<-grepl(".gz$",file_name)
+  if(zipped == T){
+    file<-fread('zcat ' %&% file_name, header = T)
+    return(file)
+  } else{
+    file<-fread(file_name, header = T)
+    return(file)
+  }
+}
+
+expr = check.fread(file=args$expression)
+
+if ( args$margin==1 ){
+	cat("treating rows as individuals\n")
+	rows<- colnames(expr)[-1]
+	cols<- unname(unlist(expr[,1]))
+	expmat = as.matrix(expr[,-1]) #margin 1 keep as is
+} else {
+	cat("treating columns as individuals\n")
+	rows<- unname(unlist(expr[,1]))
+        cols<- colnames(expr)[-1]
+        expmat = as.matrix(t(expr[,-1])) #margin 2 transpose, genes in cols, ids in rows
+}
+cat("Quantile Normalizing\n")
 qn.expmat <- as.matrix(normalize.quantiles(expmat)) ##quantile normalize
 qn<-as.matrix(t(qn.expmat))
-rownames(qn) <- expr[,1]
-colnames(qn) <- colnames(expr)[-1]
+rownames(qn) <- rows
+colnames(qn) <- cols
 write.table(file = args$outputdir %&% "/" %&% args$tag %&% "QN.txt", x = qn, quote = F, sep = '\t')
 
-#rn.qn.expmat <- apply(qn.expmat,1,"rntransform") ##rank transform to normality & transposes##
-#rownames(rn.qn.expmat) <- expr[,1]
-#colnames(rn.qn.expmat) <- colnames(expr)[-1]
-#write.table(file = "~/mets_analysis/normalized_expression/rank_normalized_splicing_chr" %&% args$tag %&% ".txt.gz", x = rn.qn.expmat, quote = F, sep = '\t')
+str(qn.expmat)
+
+cat("Rank Normalizing\n")
+rn <- apply(qn.expmat,1,"rntransform") ##rank transform to normality & transposes##
+rn.qn.expmat<-as.matrix(t(rn))
+rownames(rn) <- rows
+colnames(rn) <- cols
+write.table(file =  args$outputdir %&% "/" %&% args$tag %&% "RNQN.txt", x = rn, quote = F, sep = '\t')
+
 if (!is.null(args$cov)){
   covs = as.matrix(read.table(args$cov,header=T, row.names = 1))
   covs<-as.matrix(as.numeric(covs))
  }
   ## i is number of peer factors to calculate, recommend 25% of sample size, but no more than 100
-for(i in c(10,15,20)){
+if ( args$pilot==T ) {
+	pf_vec<-c(10)
+	cat("Performing test analysis at PF 10\n")
+} else {
+	pf_vec<-c(10,15,20)
+}
+
+for(i in pf_vec){
     model = PEER()
-    PEER_setPhenoMean(model, qn.expmat)
+    PEER_setPhenoMean(model, rn.qn.expmat)
     if (!is.null(args$cov)){
       PEER_setCovariates(model, as.matrix(covs))
     }  
@@ -48,17 +86,19 @@ for(i in c(10,15,20)){
     print(PEER_plotModel(model))
     adjexp <- t(residuals)
     print(dim(adjexp))
-    
-    rownames(adjexp) <- expr[,1]
-    colnames(adjexp) <- colnames(expr)[-1]
+    print(nrow(adjexp))
+#    str(cols)
+#    str(rows)
+    rownames(adjexp) <- rows
+    colnames(adjexp) <- cols
     adjexp <- as.data.frame(adjexp) %>% rownames_to_column('gene_id')
     png(filename =args$outputdir %&% "/" %&% args$tag %&% "_" %&% i %&% '_peer.png')
     factors = PEER_getX(model)
-    rownames(factors) <- colnames(expr)[-1]
-    write.table(factors,file=args$outputdir %&% "/" %&% args$tag %&% "_splicing_expression" %&% i %&% ".PEER.factors" %&% ".txt", quote=F)
+    rownames(factors) <- cols
+    write.table(factors,file=args$outputdir %&% "/" %&% args$tag %&% "_TOPMED_expression" %&% i %&% ".PEER.factors" %&% ".txt", quote=F,col.names=F)
     PEER_plotModel(model)
     dev.off()
-    write.table(adjexp, file=args$outputdir %&% "/" %&% args$tag %&% "_splicing_expression" %&% i %&% '_peer_factor_adjusted.txt' , quote=F, row.names=F, sep='\t')
+    write.table(adjexp, file=args$outputdir %&% "/" %&% args$tag %&% "_TOPMED_expression" %&% i %&% '_peer_factor_adjusted.txt' , quote=F, row.names=F, sep='\t')
 }
 
 
